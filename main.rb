31ff50bf47c53f0ab17c7f81666c95e1bb6991d4
Matt Lemmon <mattlemmon@gmail.com>
2013-11-22 02:21:37 -0600

    remove more un-needed files

--- main.rb
+++ main.rb
@@ -57,7 +57,7 @@ class Pause < Chingu::GameState
     pop_game_state(:setup => false)    # Return the previous game state, dont call setup()
   end  
   def draw
-    previous_game_state.draw    # Draw prev game state onto screen (in this case our level)
+    previous_game_state.draw    # Draw prev game state onto screen
     super                       # Draw game objects in current game state, this includes Chingu::Texts
   end  
 end
@@ -70,6 +70,10 @@ end
 class GameWindow < Chingu::Window
   def initialize
     super(1000,800,false)
+    @cursor = true # set to false to hide cursor
+    self.caption = "Stick Fight"
+    self.input = { :esc => :exit,
+                   :z => :log      }
     $round = 1
     $intro = true
     $pos1_x, $pos1_y = 740, 300
@@ -79,52 +83,20 @@ class GameWindow < Chingu::Window
     $scr_edge = 15
     $image1 = "boy"
     $image2 = "boy"
-#    $chime = Sound["media/audio/pickup_chime.ogg"]
-#    $chime_right = Sound["media/audio/chime_right.ogg"]
-#    $chime_left = Sound["media/audio/chime_left.ogg"]
     $click = Sound["media/audio/keypress.ogg"]
-#    $click_right = Sound["media/audio/click_right.ogg"]
-#    $click_left = Sound["media/audio/click_left.ogg"]
-    #$star_grab = Sound["media/audio/star_pickup.ogg"]
-#    $star_grab_right = Sound["media/audio/star_grab_right.ogg"]
-#    $star_grab_left = Sound["media/audio/star_grab_left.ogg"]
-#    $power_up_left = Sound["media/audio/power_up_left.ogg"]
-#    $power_up_right = Sound["media/audio/power_up_right.ogg"]
-#    $mist_grab_left = Sound["media/audio/mist_grab_left.ogg"]
-#    $mist_grab_right = Sound["media/audio/mist_grab_right.ogg"]
-#    $stun_grab_right = Sound["media/audio/stun_grab_right.ogg"]
-#    $stun_grab_left = Sound["media/audio/stun_grab_left.ogg"]
-#    $one_up_left = Sound["media/audio/1up_left.ogg"]
-#    $one_up_right = Sound["media/audio/1up_right.ogg"]
     $zapped = Sound["media/audio/magical_zap_by_qubodup.ogg"]
     $stunned = Sound["media/audio/stunned.ogg"]
     $misted = Sound["media/audio/misted.ogg"]
     $spell_cast = Sound["media/audio/magic_fireball_by_joelaudio.ogg"]
-#    $spell_cast1 = Sound["media/audio/spell_cast_right.ogg"]
-#    $spell_cast2 = Sound["media/audio/spell_cast_left.ogg"]
     $bang = Sound["media/audio/bang.ogg"]
-#    $bang1 = Sound["media/audio/bang1.ogg"]
-#    $bang2 = Sound["media/audio/bang2.ogg"]
     $game_over = Sound["media/audio/game_over.ogg"]
     $guitar_fill = Sound["media/audio/guitar_fill.ogg"]
-    #$guitar_riff = Sound["media/audio/guitar_riff_short.ogg"]
-    #$hold_music1 = Sound["media/audio/hold_music1.ogg"]
-    #$hold_music2 = Sound["media/audio/hold_music2.ogg"]
-    #$hold_music3 = Sound["media/audio/hold_music3.ogg"]
-    #$hold_music4 = Sound["media/audio/hold_music4.ogg"]
-    #$guitar_solo = Song["media/audio/guitar_solo.ogg"]
-    #$guitar_song = Song["media/audio/guitar_song.ogg"]
-    self.caption = "Stick Ball"
-    @cursor = true # comment out to hide cursor
-    self.input = { :esc => :exit,
-                   :z => :log
-                 }
+
     retrofy
   end
 
   def setup
     push_game_state(Beginning)
-#    push_game_state(CrowdScene2)
   end
 
   def log
@@ -133,12 +105,3 @@ class GameWindow < Chingu::Window
 end
 
 GameWindow.new.show
-
-
-#  def pop
-#    puts "pop"
-#    if $window.current_game_state.to_s != "Beginning"
-#      pop_game_state(:setup => true)
-#    end
-#  end
-
